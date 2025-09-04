d386f344bf97cf015eec47ce23a42d03ff639890
matias-francisco-deo <matiasfd.deo@gmail.com>
2025-09-04 15:36:23 -0300

    move to / after register

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,5 @@
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppRoutes } from "./routes/App.routes";
 
-// const queryClient = new QueryClient();
-
-export function App() {
+export default function App() {
   return <AppRoutes></AppRoutes>;
 }
--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -9,10 +9,7 @@ interface Props {
 export function MainLayout({ children }: Props) {
   return (
     <div className="flex min-h-dvh flex-col">
-      {/* <ToastContainer autoClose={5000} position="top-center" theme="dark" />
-      <Header className="sticky top-0 left-0 z-10" />
       <main className="flex-grow p-4 md:p-8">{children}</main>
-      <Footer /> */}
     </div>
   );
 }
