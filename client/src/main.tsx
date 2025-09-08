379626ae98453985dec7538962d510636ba4cd78
Marcos Diaz <ma.nahuel.d@gmail.com>
2025-09-08 01:34:28 -0300

    Frontend de crear inmuebles

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './pages/LoginPage.tsx'
+// import App from './pages/LoginPage.tsx'
+import App from './pages/createPropertyPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
