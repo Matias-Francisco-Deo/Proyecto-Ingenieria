4f26b92d7d2ab23f84a5fae5d9ed6776f4ff130d
matias-francisco-deo <matiasfd.deo@gmail.com>
2025-09-17 21:18:48 -0300

    PetitionsPage with id 1

--- client/src/components/Paginacion.tsx
+++ client/src/components/Paginacion.tsx
@@ -1,38 +1,38 @@
 interface PaginacionProps {
-    paginaActual: number;
-    totalPaginas: number;
-    onPageChange?: (page: number) => void;
+  paginaActual: number;
+  totalPaginas: number;
+  onPageChange?: (page: number) => void;
 }
 
 export default function Paginacion({
-    paginaActual,
-    totalPaginas,
-    onPageChange,
+  paginaActual,
+  totalPaginas,
+  onPageChange,
 }: PaginacionProps) {
-    return (
-        <div className="flex items-center justify-center gap-4">
-            {/* Botón anterior */}
-            <button
-                onClick={() => onPageChange?.(paginaActual - 1)}
-                disabled={paginaActual === 1}
-                className="px-3 py-1 rounded bg-amber-600 text-white disabled:bg-gray-400"
-            >
-                atras
-            </button>
+  return (
+    <div className="flex items-center justify-center gap-4">
+      {/* Botón anterior */}
+      <button
+        onClick={() => onPageChange?.(paginaActual - 1)}
+        disabled={paginaActual === 1}
+        className="px-3 py-1 rounded bg-amber-600 text-white disabled:bg-gray-400"
+      >
+        Atrás
+      </button>
 
-            {/* Número actual */}
-            <span className="px-4 py-1 rounded bg-gray-200 text-black font-bold">
-                {paginaActual} / {totalPaginas}
-            </span>
+      {/* Número actual */}
+      <span className="px-4 py-1 rounded bg-gray-200 text-black font-bold">
+        {paginaActual} / {totalPaginas}
+      </span>
 
-            {/* Botón siguiente */}
-            <button
-                onClick={() => onPageChange?.(paginaActual + 1)}
-                disabled={paginaActual === totalPaginas}
-                className="px-3 py-1 rounded bg-amber-600 text-white disabled:bg-gray-400"
-            >
-                adelante
-            </button>
-        </div>
-    );
+      {/* Botón siguiente */}
+      <button
+        onClick={() => onPageChange?.(paginaActual + 1)}
+        disabled={paginaActual === totalPaginas}
+        className="px-3 py-1 rounded bg-amber-600 text-white disabled:bg-gray-400"
+      >
+        Adelante
+      </button>
+    </div>
+  );
 }
