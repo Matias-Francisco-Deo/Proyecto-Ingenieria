636ed683f5029a96c5691e04bf5194c68f407f28
matias-francisco-deo <matiasfd.deo@gmail.com>
2025-10-02 11:35:29 -0300

    Add toastify

--- client/src/layouts/MainLayout.tsx
+++ client/src/layouts/MainLayout.tsx
@@ -1,7 +1,4 @@
-// import { Footer } from "@/components/Footer";
-// import { Header } from "@/components/Header";
-// import { ToastContainer } from "react-toastify";
-
+import { ToastContainer } from "react-toastify";
 import { Header } from "../components/Header";
 
 interface Props {
@@ -11,6 +8,7 @@ interface Props {
 export function MainLayout({ children }: Props) {
   return (
     <div className="flex min-h-dvh flex-col">
+      <ToastContainer autoClose={3000} position="top-center" theme="dark" />
       <Header className="sticky top-0 left-0 z-10 h-30 bg-amber-600 p-4 flex flex-row gap-20" />
       <main className="flex-grow p-4 md:p-8">{children}</main>
     </div>
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,22 +1,28 @@
 import BuscadorDeInmuebles from "@/components/buscador/Buscador";
 
 import { useBusquedaInmuebles } from "@/hooks/useBusquedaInmuebles";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 export default function HomePage() {
-    const busquedaProps = useBusquedaInmuebles();
-    const { hasResults } = busquedaProps;
+  const busquedaProps = useBusquedaInmuebles();
+  const { hasResults } = busquedaProps;
 
-    return (
-        <div className="flex flex-col items-center justify-center min-h-[calc(100vh-64px)] text-white p-4">
-            {!hasResults && (
-                <h1 className="text-6xl font-light text-white w-1/2 text-center">
-                    RESERVO
-                </h1>
-            )}
+  useEffect(() => {
+    toast.warn("NO");
+  }, []);
 
-            <div className="w-full max-w-2xl">
-                <BuscadorDeInmuebles {...busquedaProps} />
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-64px)] text-white p-4">
+      {!hasResults && (
+        <h1 className="text-6xl font-light text-white w-1/2 text-center">
+          RESERVO
+        </h1>
+      )}
+
+      <div className="w-full max-w-2xl">
+        <BuscadorDeInmuebles {...busquedaProps} />
+      </div>
+    </div>
+  );
 }
