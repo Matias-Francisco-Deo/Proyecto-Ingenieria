4efcdad091bd66da3716815331e8bc322c56420f
Hector Joel <joel.c.98@hotmail.com>
2025-10-09 11:18:30 -0300

    fix calculo politica severa

--- server/src/main/java/com/reservo/modelo/property/Severo.java
+++ server/src/main/java/com/reservo/modelo/property/Severo.java
@@ -10,7 +10,7 @@ public class Severo extends PoliticaDeCancelacion {
     public double calcularReintegro(double monto, Long diasAnticipacion){
         if (diasAnticipacion > 30) {
             return monto;
-        } else if (diasAnticipacion >= 7) {
+        } else if (diasAnticipacion > 7) {
             return monto * 0.5;
         } else {
             return 0;
@@ -21,7 +21,7 @@ public class Severo extends PoliticaDeCancelacion {
     public double calcularDeuda(double monto, Long diasAnticipacion){
         if (diasAnticipacion > 30) {
             return 0;
-        } else if (diasAnticipacion >= 7) {
+        } else if (diasAnticipacion > 7) {
             return monto * 0.5;
         } else {
             return monto;
